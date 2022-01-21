782429354a2acd20aa9cbe364b96f673fe53e3bc
Gustavo Henrique Xavier de Brito <ymooncake@gmail.com>
2022-01-21 17:28:10 -0300

    Update advinha.py

--- Projeto/advinha.py
+++ Projeto/advinha.py
@@ -1,6 +1,3 @@
-from webbrowser import Elinks
-
-
 print(45*'=')
 print('=========Seja bem vindo ao meu jogo!=========')
 print(45*'=')
@@ -10,16 +7,27 @@ print('Temos aqui um número escondido, caso advinhe qual é, avançará para o 
 #Declaração da variável que seta o número resposta
 numero = 43
 
-#Input que recebe a reposta do usuário
-resposta = int(input('Chute um número:\n'))
+#Declaração da variável que define quantas tentativas ainda restam pro usuário
+numeroDeTentativas = 3
 
 #Função que compara as duas variáveis(numero e resposta) e retorna se o usuário acertou ou não
-def verificaNum(resposta,numero):
-    if numero == resposta:
-        print('Parabén, aperte enter para avançar ao próximo nível\n')
-    else:
-        print('Tente...um outro valor\n')
+def verificaNum(numero):
 
-#Chamada da função que verifica as respostas
-verificaNum(resposta,numero)
+    #Input que recebe a reposta do usuário
+    resposta = int(input('Chute um número:\n'))
     
+    if numero == resposta:
+        print('Parabéns, aperte enter para avançar ao próximo nível\n')
+    elif numero > resposta:
+        print('Tente chutar um pouco mais alto\n')
+    elif numero < resposta:
+         print('tente chutar um pouco mais baixo\n')
+
+#Laço que permite novas tentativas
+while numeroDeTentativas > 0:
+    #Chamada da função que verifica o número digitado
+    verificaNum(numero)
+    numeroDeTentativas -= 1
+
+    if numeroDeTentativas == 0:
+        print('Tente novamente mais tarde')
