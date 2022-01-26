0ed33f4d82ca78fff644e7e74abd834bc437270a
Gustavo Henrique Xavier de Brito <ymooncake@gmail.com>
2022-01-26 14:20:13 -0300

    Update conta.py

--- POO/conta.py
+++ POO/conta.py
@@ -2,10 +2,10 @@
 class Conta(object):
     #Contrutor do objeto
     #Self é a referência na memória onde o objeto está
-    def __init__(self):
+    def __init__(self, numero, nome, saldo,limite):
         #Acessando atributo numero do objeto
         # Uso de 2 underlines para 'privar a variavel' ocultando ela de outras classes 
-        self.__numero = 123
-        self.nome = 'Jorge'
+        self.numero = numero
+        self.nome = nome
         self.saldo = 500
         self.limite = 1000
