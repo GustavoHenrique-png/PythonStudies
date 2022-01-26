a22a2e6ad3ba706cafaf62de82c2ef6e4f934795
Gustavo Henrique Xavier de Brito <ymooncake@gmail.com>
2022-01-26 16:23:59 -0300

    Acessando atributos

--- POO/conta.py
+++ POO/conta.py
@@ -3,9 +3,21 @@ class Conta(object):
     #Contrutor do objeto
     #Self é a referência na memória onde o objeto está
     def __init__(self, numero, nome, saldo,limite):
+        #__init__ é o que marca o construtor da função
         #Acessando atributo numero do objeto
         # Uso de 2 underlines para 'privar a variavel' ocultando ela de outras classes 
         self.numero = numero
         self.nome = nome
-        self.saldo = 500
-        self.limite = 1000
+        self.saldo = saldo
+        self.limite = limite
+    def extrato(self):
+        print('O saldo do: {} é de: ${}'.format(self.nome,self.saldo))
+    def deposito(self, valor):
+        self.saldo += valor
+    def saque(self,valor):
+        self.saldo -= valor
+
+conta = Conta(123,'horge',500,1000)
+conta2 = Conta(321,'teo',20,1000)
+
+print(conta.extrato())
